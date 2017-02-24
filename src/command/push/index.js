b00c7136838a388ef738bb54ca015a90e0d57334
Damien Bidaud <damien.bidaud@eurostar.com>
2017-02-24 16:10:51 +0000

    master: test

--- src/command/push/index.js
+++ src/command/push/index.js
@@ -11,6 +11,7 @@ class Push {
   execute() {
     this.repo.branch((err, head)=>{
       if(err) console.error(err);
+      console.log('k');
       this.repo.remote_push(`origin ${head.name}`, (err)=>{
         if(err) console.error(err);
       })
