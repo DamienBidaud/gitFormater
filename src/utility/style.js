5e62d4602ef565e8f7b3afef6e5807c1a2508bda
Damien Bidaud <damien.bidaud@eurostar.com>
2017-02-24 22:04:27 +0000

    master: update version package 1.0.3

--- src/utility/style.js
+++ src/utility/style.js
@@ -1,3 +1,35 @@
 /**
  * Created by bidaudd on 24/02/2017.
  */
+'use strict';
+
+const chalk = require('chalk')
+
+class OutputStyle {
+   static red(message) {
+    console.log(chalk.red(message));
+   }
+
+   static valid(message) {
+    console.log(chalk.green.bold(message));
+   }
+
+   static green(message) {
+    console.log(chalk.green(message));
+   }
+
+  static title(message) {
+   console.log(chalk.bold.underline(message));
+  }
+
+  static question(message) {
+    return chalk.cyan.bold(message);
+  }
+
+  static warning(message) {
+    console.log(chalk.yellow.bold(message));
+  }
+}
+
+
+module.exports = OutputStyle;
